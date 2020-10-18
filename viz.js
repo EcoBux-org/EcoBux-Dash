4392598f636b10ec5b2939ad36f196d832a6a2a7
Watt3r <lucas@ecobux.org>
2020-10-18 14:59:01 -0700

    Add support for testing on goerli

--- viz.js
+++ viz.js
@@ -1,43 +1,53 @@
 // NOTE: Any updates need to compile with browserify viz.js -o main.js
-var SolidityCoder = require("web3/lib/solidity/coder.js");
-
-network = "dev"
+const Web3 = require("web3");
 
 // Different contracts for each dev environment 
-// Mainnet will have a static address but the ABI file will still need to be loaded
-$.ajaxSetup({
-    async: false
-});
-// Get contract ABI and address
-$.getJSON("../EcoBux/build/contracts/Piloto.json", function(build) {
-  Piloto = build.networks[Object.keys(build.networks)[0]].address;
-  pilotoAbi = build.abi;
-  console.log("Piloto contract address: " + Piloto);
-})
-$.getJSON("../EcoBux/build/contracts/EcoBux.json", function(build) {
-  ECOBcontractAddress = build.networks[Object.keys(build.networks)[0]].address;
-  ecobAbi = build.abi;
-  console.log("EcoBux contract address: " + ECOBcontractAddress);
-})
-$.ajaxSetup({
-    async: true
-});
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+network = "gorli" // local ; gorli ; main
+if (network === "local") {
+  // Mainnet will have a static address but the ABI file will still need to be loaded
+  $.ajaxSetup({
+      async: false
+  });
+  // Get contract ABI and address
+  $.getJSON("build/contracts/Piloto.json", function(build) {
+    Piloto = build.networks[Object.keys(build.networks)[0]].address;
+    PilotoAbi = build.abi;
+    console.log("Piloto contract address: " + Piloto);
+  })
+  $.getJSON("build/contracts/EcoBux.json", function(build) {
+    ECOBcontractAddress = build.networks[Object.keys(build.networks)[0]].address;
+    EcobAbi = build.abi;
+    console.log("EcoBux contract address: " + ECOBcontractAddress);
+  })
+  $.ajaxSetup({
+      async: true
+  });
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8546"));
+} else if (network==="gorli") {
 
-var accounts = web3.eth.accounts;
+  Piloto = "0x2537e4F98C462766851b66986c913Cc4d9338362"
+  ECOBcontractAddress = "0xC1122117A777eC07286ecaa353A6fEb36B08AeAf"
+
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8546"));
+
+} else if (network==="main") {
+  console.error("Main network not deployed yet, use gorli or local blockchain instead")
+} else {
+  console.error("Network name not recognized.")
+}
+// var web3 = new Web3(new GSNProvider("http://localhost:8546"));
+
 // Setting user address
-var account = accounts[0];
-
-web3.eth.defaultAccount = account;
-
-// Assemble function hashes
-
-var functionHashes = getFunctionHashes(pilotoAbi);
+web3.eth.getAccounts((err, res) => {
+  accounts = res
+  account = accounts[0];
+  web3.eth.defaultAccount = account;
+})
 
 // Get hold of contract instance
 
-var contract = web3.eth.contract(pilotoAbi).at(Piloto);
-var ecob = web3.eth.contract(ecobAbi).at(ECOBcontractAddress);
+var contract = new web3.eth.Contract(PilotoAbi, Piloto);
+var ecob = new web3.eth.Contract(EcobAbi, ECOBcontractAddress);
 
 // User functions
 
@@ -66,15 +76,12 @@ function apprEcob() {
   var amount = document.getElementById('apprEcobAmount').value;
   var from = $('#address').val();
   
-  ecob.approve(to, amount, {from: from}, function(err, txHash){
-    let txReceipt = web3.eth.getTransactionReceipt(txHash);
-    // txReceipt.logs contains an array of all events fired while calling your fxn
-    console.log(err, txReceipt);
-    var log = txReceipt.logs[0];
-    var data = SolidityCoder.decodeParams(
-                ["address", "uint"], log.data.replace("0x", "")
-               );
-    console.log(data);
+  ecob.methods.approve(to, amount).send({from: account}).on('receipt', function(receipt){
+    console.log("Successfully approved " + amount + " EcoBux to the Piloto contract");
+    console.log(receipt)
+  })
+  .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+    console.error(error)  
   });
 }
 
@@ -85,17 +92,12 @@ function buyEcoBlock() {
   var amount = document.getElementById('buyBlockAmount').value;
   var account = $('#address').val();
   
-  // 1500 is cost (in decimals) of one EcoBlock
-  contract.buyEcoBlocks(amount, account, {from:account,gas:8000000}, function(err, txHash){
-    let txReceipt = web3.eth.getTransactionReceipt(txHash);
-    // txReceipt.logs contains an array of all events fired while calling your fxn
-    console.log(err, txReceipt);
-    var log = txReceipt.logs[3];
-    var data = SolidityCoder.decodeParams(
-                ["address", "address", "uint"], log.data.replace("0x", "")
-               );
-    console.log(data);
+  contract.methods.buyEcoBlocks(amount, account).send({from: account}).on('receipt', function(receipt){
     console.log("Purchased EcoBlock(s)") 
+    console.log(receipt)
+  })
+  .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+    console.error(error)  
   });
 }
 
@@ -109,36 +111,32 @@ function buyAddon() {
 
 // Testing Functions
 
-// This should be called after user pays with paypal
 $('#mintEcob').click(mintEcob);
 function mintEcob() {
   var addr = document.getElementById('mintEcobAddr').value;
   var amount = document.getElementById('mintEcobAmount').value;
-  console.log(addr, amount);
 
-  ecob.createEco(addr, amount, {from: accounts[0]}, function(err, txHash){
-    let txReceipt = web3.eth.getTransactionReceipt(txHash);
-    // txReceipt.logs contains an array of all events fired
-    console.log(txReceipt);
+  ecob.methods.createEco(addr, amount).send({from: accounts[0]}).on('receipt', function(receipt) {
+    console.log("Successfully given " + addr + " " + amount + " Ecobux");
+    console.log(receipt);
+  })
+  .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+    console.error(error)  
   });
 }
 
 // Function while initializing contract to load all EcoBlocks
 $('#createEcoBlock').click(createEcoBlock);
 function createEcoBlock() {
-  $.getJSON("allotments.json", function(blockArray) {
-    // Only add first 10 allotments
+  $.getJSON("EcoBlocks.json", function(blockArray) {
+    // Only add first 10 EcoBlocks 
     blockArray = blockArray.slice(0,10)
 
-    contract.bulkCreateEcoBlocks(blockArray, {from: accounts[0], gas: 8000000}, function(err, txHash) {
-      let txReceipt = web3.eth.getTransactionReceipt(txHash);
-      // txReceipt.logs contains an array of all events fired
-      var log = txReceipt.logs[0];
-      if (txReceipt.status == 1) {
-        console.log("Successfully created 10 EcoBlocks, given to the Piloto contract");
-      } else {
-        console.log(err);
-      }
+    contract.methods.bulkCreateEcoBlocks(blockArray).send({from: accounts[0]}).on('receipt', function(receipt) {
+      console.log("Successfully created 10 EcoBlocks, given to the Piloto contract");
+    })
+    .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+      console.error(error)  
     });
   });
 }
@@ -149,25 +147,37 @@ function createAddon() {
   var price = document.getElementById('createAddonPrice').value;
   var purchasable = document.getElementById('createAddonPurchasable').value;
 
-  contract.createMicro(price, purchasable, {from: accounts[0]}, function(err, txHash){
-    let txReceipt = web3.eth.getTransactionReceipt(txHash);
-    // txReceipt.logs contains an array of all events fired while calling your fxn
-    console.log(txReceipt);
-    var log = txReceipt.logs[0];
-    var data = SolidityCoder.decodeParams(
-                ["uint", "uint", "bool"], log.data.replace("0x", "")
-               );
-    console.log(data);
+  contract.methods.createMicro(price, purchasable).send({from: accounts[0]}).on('receipt', function(receipt) {
+    console.log("Successfully created microaddon");
+  })
+  .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+    console.error(error)  
   });
-
 }
 
+// Function while initializing contract to load all EcoBlocks
+$('#giveEcoBlock').click(giveEcoBlock);
+function giveEcoBlock() {
+  var amount = document.getElementById('giveBlockAmount').value;
+  var account = document.getElementById('giveBlockAddr').value;
+  
+  // 1500 is cost (in decimals) of one EcoBlock
+  contract.methods.giveEcoBlocks(amount, account).send({from: accounts[0]}).on('receipt', function(receipt) {
+    console.log(`Successfully gave ${amount} Ecoblocks`);
+  })
+  .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+    console.error(error)  
+  });
+}
+
+// List all owned EcoBlocks
 $('#ownedEcoBlocks').click(ownedEcoBlocks);
 function ownedEcoBlocks() {
   var addr = document.getElementById('ownedBlockAddr').value;
-  console.log(contract.ownedEcoBlocks(addr))
+  contract.methods.ownedEcoBlocks(addr).call().then(function(owned) {
+    console.log(owned);
+  });
 }
-
 
 // Non state modifiying functions
 // Update labels every second
@@ -176,49 +186,31 @@ setInterval(function() {
   var account = $('#address').val();
   
   // Account balance in Ether
-  var balanceWei = web3.eth.getBalance(account).toNumber();
-  var balance = web3.fromWei(balanceWei, 'ether');
-  $('#label1').text(balance);
+  web3.eth.getBalance(account).then(function(weibal) {
+    $('#label1').text(web3.utils.fromWei(weibal, 'ether'));
+  });
 
   // Block number
-  var number = web3.eth.blockNumber;
-  if ($('#label2').text() != number)
-    $('#label2').text(number).effect("highlight");
+  web3.eth.getBlockNumber(function(error, result){
+    if (!error) {
+      if ($('#label2').text() != result)
+        $('#label2').text(result).effect("highlight");
+    }
+  });
   
   // Print account address (not state changing)
   $('#label3').val(account);
   
   // Check ecobucks balance: call (not state changing)
-  var ecobAllowed = ecob.allowance(account, Piloto) / 100;
-  var ecobBalance = ecob.balanceOf(account) / 100;
-  $('#label4').text(ecobBalance + " (Owned)   " + ecobAllowed + " (Approved to contract)");
+  ecob.methods.balanceOf(account).call().then(function(bal, ecobAllowed) {
+    $('#label4-1').text(bal / 100+ " (Owned)   ");
+  })
+  ecob.methods.allowance(account, Piloto).call().then(function(allowed) {
+    $('#label4-2').text(allowed / 100 + " (Approved to contract)");
+  })
+
 
 }, 1000);
-
-// Get function hashes
-// TODO: also extract input parameter types for later decoding
-
-function getFunctionHashes(abi) {
-  var hashes = [];
-  for (var i=0; i<abi.length; i++) {
-    var item = abi[i];
-    if (item.type != "function") continue;
-    var signature = item.name + "(" + item.inputs.map(function(input) {
-      return input.type;
-    }).join(",") + ")";
-    var hash = web3.sha3(signature);
-    hashes.push({name: item.name, hash: hash});
-  }
-  return hashes;
-}
-
-function findFunctionByHash(hashes, functionHash) {
-  for (var i=0; i<hashes.length; i++) {
-    if (hashes[i].hash.substring(0, 10) == functionHash.substring(0, 10))
-      return hashes[i].name;
-  }
-  return null;
-}
 
 $( document ).ready(function() {
   var optionsAsString = "";
